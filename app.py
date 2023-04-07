056e1f1721b69b251dbc22ec9951f099e0986865
wiwin winingsih <winingsih991@gmail.com>
2023-04-07 12:49:55 +0700

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-import os                          
-from os.path import join,  dirname
-from dotenv import load_dotenv 
-
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-dotenv_path = join(dirname(__file__),'.venv')
+import os
+from os.path import join,dirname
+from dotenv import load_dotenv
+
+
+
+dotenv_path =join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
-MONGODB_URI =os.environ.get("MONGODB_URI")
-DB_NAME=os.environ.get("DB_NAME")
+
+MONGODB_URI=os.environ.get("MONGODB_URI")
+DB_NAME =os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
@@ -22,15 +25,15 @@ def home():
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
     bucket_receive = request.form['bucket_give']
-    count = db.bucket.count_documents({})
+    count =db.bucket.count_documents({})
     num = count + 1
     doc = {
-        'num': num,
+        'num':num,
         'bucket': bucket_receive,
-        'done': 0
+        'done':0
     }
     db.bucket.insert_one(doc)
-    return jsonify({'msg': 'data saved!'})
+    return jsonify({'msg':'data saved!'})
 
 @app.route("/bucket/done", methods=["POST"])
 def bucket_done():
@@ -39,20 +42,21 @@ def bucket_done():
         {'num': int(num_receive)},
         {'$set': {'done': 1}}
     )
-    return jsonify({'msg': 'update done!'})
+    return jsonify({'msg': 'Update done!'})
+
+
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
-    buckets_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': buckets_list})
-
+    buckets_list = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'buckets':buckets_list})
 
 @app.route("/bucket/delete", methods=["POST"])
 def bucket_delete():
     num_receive = request.form["num_give"]
-    db.bucket.delete_one({'num': int(num_receive)})
-    buckets_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'msg': 'Delete done!', 'buckets': 'buckets_list'})
-
-if __name__ == '__main__':
+    db.bucket.delete_one({'num': int (num_receive)})
+    bucket_list= list(db.bucket.find({},{'_id':False}))
+    return jsonify({'msq':'Delete done!','buckets':'bucket_list'})
+    
+if __name__== '_main_':
    app.run('0.0.0.0', port=5000, debug=True)
